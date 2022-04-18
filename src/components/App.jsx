6193fdabcbcc50a01a624b8674ea05225357e728
Halil_Akgun <halil.akgun@renault.com.tr>
2022-04-18 09:57:28 +0300

    add image support to webpack

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
 import { render } from 'react-dom'
 import '../styles/index.scss'
 import Recipe from './Recipe'
+import sword from '../images/swc-sword.png'
+import swordSvg from '../images/sword.svg'
+
 const App = () => {
     return (
         <>
             <section className="hero">
-                <main>
-                    <section>
-                        <h1>Oh Haiaa, React</h1>
-                    </section>
-                </main>
             </section>
+            <main>
+                <section>
+                    <h1>Oh Haiaa, React</h1>
+                </section>
+                <img src={sword} alt="sword" width="250" />
+                <img src={swordSvg} alt="swordSvg" width="250" />
+            </main>
             <Recipe />
         </>
     )
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     // mode: mode,
+    output: {
+        assetModuleFilename: 'images/[hash][ext][query]',
+    },
     devServer: {
         static: {
             directory: path.resolve(__dirname, 'dist')
@@ -12,6 +15,15 @@ module.exports = {
     devtool: "source-map",
     module: {
         rules: [
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/i,
+                type: 'asset',
+                // parser: {
+                //     dataUrlCondition: {
+                //         maxSize: 30 * 1024, // normal size is 8kb
+                //     }
+                // }
+            },
             {
                 test: /\.(css|scss|sass)$/i,
                 use: [
